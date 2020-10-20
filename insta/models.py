779ae80ca4ed36ce46112238646714385281d944
PatriciaAnduru <andurupatricia@gmail.com>
2020-10-20 18:32:55 +0300

    initial commit

---
+++ insta/models.py
@@ -0,0 +1,93 @@
+from django.contrib.auth.models import User
+from django.db import models
+from pyuploadcare.dj.models import ImageField
+from friendship.models import Friend,Follow,Block
+from django.utils import timezone
+
+
+class Profile(models.Model):
+    profile_pic =ImageField( blank=True)
+    bio = models.CharField(max_length=255)
+    owner = models.OneToOneField(User,blank=True, on_delete=models.CASCADE, related_name="profile")
+
+    def __str__(self):
+        return str(self.bio)
+
+
+    def profile_save(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(owner=id)
+        return profile
+
+    @classmethod
+    def get_profile_by_username(cls, owner):
+        profiles = cls.objects.filter(owner__contains=owner)
+        return profiles
+
+
+
+
+
+class Image(models.Model):
+    pic=ImageField(manual_crop='1080x800', blank=True)
+    name= models.CharField(max_length=55)
+    caption = models.TextField(blank=True)
+    profile= models.ForeignKey(User, blank=True,on_delete=models.CASCADE)
+    profile_details = models.ForeignKey(Profile)
+
+
+
+    def __str__(self):
+        return str(self.name)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_images(cls, profile):
+        images = Image.objects.filter(profile__pk=profile)
+        return images
+
+class Comment(models.Model):
+    image = models.ForeignKey(Image,blank=True, on_delete=models.CASCADE,related_name='comment')
+    comment_owner = models.ForeignKey(User, blank=True)
+    comment= models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    @classmethod
+    def get_image_comments(cls, id):
+        comments = Comment.objects.filter(image__pk=id)
+        return comments
+
+    def __str__(self):
+        return str(self.comment)
+
+
+
+class Likes(models.Model):
+    liker=models.ForeignKey(User)
+    image =models.ForeignKey(Image)
+
+
+
+
+
+
+
